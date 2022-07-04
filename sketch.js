2bd0467156bb78a6cc96192d47052b1419203326
Arnav <107131964+ArnavSinghDeopa@users.noreply.github.com>
2022-07-04 06:13:17 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var sc;
+function setup() {
+  createCanvas(400,400);
+//creating a object of a student class
+sc = new Student("Name-Arnav","Class-8","Age-13");
+sc.display();
+}
+
+function draw() 
+{
+  background(30);
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
